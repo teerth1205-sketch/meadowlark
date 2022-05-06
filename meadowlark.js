d430797c374dd926416e3190058ea9c21d546455
Teerth Patel <teerthpatel@Teerths-MacBook-Pro-2.local>
2022-05-06 13:07:17 -0700

    first commit

---
+++ meadowlark.js
@@ -0,0 +1,71 @@
+var express = require('express');
+
+var app = express();
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
+
+var handlebars = require('express3-handlebars')
+ .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+   
+app.get('/about', function(req, res){
+    var randomFortune =
+    fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+   // 404 catch-all handler (middleware)
+   
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+   });
+   // 500 error handler (middleware)
+   
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+// app.get('/', function(req, res){
+//     res.type('text/plain');
+//     res.send('Meadowlark Travel');
+// });
+
+// app.get('/about', function(req, res){
+//     res.type('text/plain');
+//     res.send('About Meadowlark Travel');
+// });
+
+// app.use(function(req, res){
+//     res.type('text/plain');
+//     res.status(404);
+//     res.send('404 - Not Found');
+// });
+//    // custom 500 page
+// app.use(function(err, req, res, next){
+//     console.error(err.stack);
+//     res.type('text/plain');
+//     res.status(500);
+//     res.send('500 - Server Error');
+// });
+   
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+    console.log(process.env.PORT)
+});
